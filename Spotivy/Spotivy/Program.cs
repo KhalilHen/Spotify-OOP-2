461b807fc0c9ac0a1aa575ff4e87917819209ab0
bleenie <boleenders01@gmail.com>
2024-06-13 12:12:14 +0200

    interactive menu wip

--- Spotivy/Spotivy/Program.cs
+++ Spotivy/Spotivy/Program.cs
@@ -6,8 +6,24 @@ namespace Spotivy
     {
         static void Main(string[] args)
         {
+            User user1 = new User("Bo", "1234");
+            User user2 = new User("Khalil", "5678");
+            User user3 = new User("Robert", "9012");
+
             Console.WriteLine("Welcome to Spotivy!");
-            Console.WriteLine("Who is listening?");
+            Console.WriteLine("\nWho is listening?");
+
+
+            ConsoleKeyInfo key;
+
+            while (true)
+            {
+            Console.WriteLine("\u001b[32m" + user1.getName() + "\u001b[0m");
+            Console.WriteLine(user2.getName());
+            Console.WriteLine(user3.getName());
+
+                key = Console.ReadKey(true);
+            }
         }
     }
 }
--- Spotivy/Spotivy/User.cs
+++ Spotivy/Spotivy/User.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Spotivy
 {
-    internal class User
+    internal class User(String name, String password)
     {
+        int id;
+        String name = name;
+        private String password = password;
+
+        public String getName()
+        {
+            return name;
+        }
     }
 }
